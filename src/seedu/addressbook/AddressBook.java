91468666f3eb23b28475becf32b0453383ec7ec5
Fyelight <kaid.fyelight@hotmail.com>
2017-01-25 13:38:52 +0800

    Rename changed to isChanged

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -51,7 +51,7 @@ public class AddressBook {
      */
     private static final String LINE_PREFIX = "|| ";
 
-    /**
+    /**357
      * A platform independent line separator.
      */
     private static final String LS = System.lineSeparator() + LINE_PREFIX;
@@ -794,11 +794,11 @@ public class AddressBook {
      * @return true if the given person was found and deleted in the model
      */
     private static boolean deletePersonFromAddressBook(String[] exactPerson) {
-        final boolean changed = ALL_PERSONS.remove(exactPerson);
-        if (changed) {
+        final boolean isChanged = ALL_PERSONS.remove(exactPerson);
+        if (isChanged) {
             savePersonsToFile(getAllPersonsInAddressBook(), storageFilePath);
         }
-        return changed;
+        return isChanged;
     }
 
     /**
